918487cd4f21b840b19ce8c448c12e17698170ce
Zack A Claar <claar.zack@gmail.com>
2019-02-16 07:38:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  } else 
+  } else if (string === "I love you, Grandma.") {
+    return 
+  }
 }
 
