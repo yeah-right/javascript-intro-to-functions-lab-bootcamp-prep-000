d683959fcb804972bc2adfd287bf89f327470705
Zack A Claar <claar.zack@gmail.com>
2019-02-16 07:36:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-// function shout(string) {
-//   return string.toUpperCase()
-// }
+function shout(string) {
+  return string.toUpperCase()
+}
 
-// function whisper(string) {
-//   return string.toLowerCase()
-// }
+function whisper(string) {
+  return string.toLowerCase()
+}
 
-// function logShout(string) {
-//   console.log(string.toUpperCase())
-// }
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
 
-// function logWhisper(string) {
-//   console.log(string. toLowerCase())
-// }
+function logWhisper(string) {
+  console.log(string. toLowerCase())
+}
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
-    console.log("I can't hear you!")
+    return "I can't hear you!"
   }
 }
 
-sayHiToGrandma("hello")
